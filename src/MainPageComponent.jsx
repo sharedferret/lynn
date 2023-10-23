023eec897b1127341730fe11620cd4fd097a9673
Kate Wellington <kate@katewellington.com>
2023-10-23 09:15:56 -0700

    Add new homepage images

--- src/MainPageComponent.jsx
+++ src/MainPageComponent.jsx
@@ -5,6 +5,64 @@ import {
 import { Helmet } from 'react-helmet';
 
 export default function MainPageComponent() {
+  function daysIntoYear(date) {
+    return (
+      Date.UTC(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+      ) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+  }
+
+  function getPageThemeData() {
+    const themeOptions = [
+      {
+        image: `${process.env.PUBLIC_URL}/assets/lynn.jpg`,
+        imageCredit: 'Julian Mamanen',
+        imageCreditLink: 'https://twitter.com/donotfeedlala',
+        color: '#ffb0a9',
+      },
+      {
+        image: `${process.env.PUBLIC_URL}/assets/bg2.jpg`,
+        imageCredit: 'Temmie',
+        imageCreditLink: 'https://twitter.com/tuyoki',
+        color: '#e7fae2',
+      },
+      {
+        image: `${process.env.PUBLIC_URL}/assets/bg3.jpg`,
+        imageCredit: 'コマ🍀',
+        imageCreditLink: 'https://www.tumblr.com/komama',
+        color: '#e8c3ad',
+      },
+      {
+        image: `${process.env.PUBLIC_URL}/assets/bg4.jpg`,
+        imageCredit: 'つなこーん',
+        imageCreditLink: 'https://www.pixiv.net/en/users/215544',
+        color: '#ffede8',
+      },
+      {
+        image: `${process.env.PUBLIC_URL}/assets/bg5.jpg`,
+        imageCredit: 'みーしゃ/19',
+        imageCreditLink: 'https://twitter.com/misiaaa19',
+        color: '#bcd7ea',
+      },
+      {
+        image: `${process.env.PUBLIC_URL}/assets/bg6.jpg`,
+        imageCredit: 'そらに',
+        imageCreditLink: 'https://twitter.com/srn_111',
+        color: '#bbbbb8',
+      },
+      {
+        image: `${process.env.PUBLIC_URL}/assets/bg7.jpg`,
+        imageCredit: 'Shirogane Ryo',
+        imageCreditLink: 'https://twitter.com/shiroganeryo',
+        color: '#e2ced1',
+      },
+    ];
+    const dayOfMonth = daysIntoYear(new Date());
+    return themeOptions[(dayOfMonth) % themeOptions.length];
+  }
+
   const renderIntroParagraph = ((textSize) => (
     <Paper elevation={3}>
       <Stack p={5} spacing={2}>
@@ -34,6 +92,14 @@ export default function MainPageComponent() {
           Built by Lynn Kaneko @ Exodus. Issues/suggestions? You can reach out to me
           on Discord (@lynnkaneko).
         </Typography>
+        <Typography textAlign="left" fontSize={textSize}>
+          Home page artwork by
+          {' '}
+          <a href={getPageThemeData().imageCreditLink}>
+            {getPageThemeData().imageCredit}
+            !
+          </a>
+        </Typography>
       </Stack>
     </Paper>
   ));
@@ -45,7 +111,7 @@ export default function MainPageComponent() {
    */
   return (
     <Box flexGrow={1} height="100%" sx={{ pt: { xs: 14, md: 5 } }}>
-      <Helmet bodyAttributes={{ style: 'background-color : #ffb0a9' }}>
+      <Helmet bodyAttributes={{ style: `background-color : ${getPageThemeData().color}` }}>
         <meta name="description" content="A collection of tools for Final Fantasy XIV side content created by Lynn Kaneko @ Exodus" />
         <meta property="og:title" content="lynn.pet!" />
         <meta property="og:url" content="https://lynn.pet/" />
@@ -65,9 +131,9 @@ export default function MainPageComponent() {
             {renderIntroParagraph(18)}
           </Box>
 
-          <Box maxWidth="70%">
+          <Box maxWidth="70%" p={2}>
             <img
-              src={`${process.env.PUBLIC_URL}/assets/lynn.jpg`}
+              src={getPageThemeData().image}
               alt="lynn.pet!"
             />
           </Box>
@@ -79,7 +145,7 @@ export default function MainPageComponent() {
           {renderIntroParagraph(20)}
           <Box width="100%">
             <img
-              src={`${process.env.PUBLIC_URL}/assets/lynn.jpg`}
+              src={getPageThemeData().image}
               alt="lynn.pet!"
             />
           </Box>
