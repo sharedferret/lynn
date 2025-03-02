56a71bd7edd6a0779cfde0995aa936d56bca3e73
Kate Wellington <kate@katewellington.com>
2025-03-02 12:40:56 -0800

    Fix trailing space

--- src/map/MapLayerSelectorComponent.jsx
+++ src/map/MapLayerSelectorComponent.jsx
@@ -78,7 +78,7 @@ export default function MapLayerSelectorComponent({
     checkboxesToDisplay.push(
       <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
         {categoryLayers}
-      </Box>, 
+      </Box>,
     );
   }
 
